da889fed24158bf0f88d486ac7d9c65ce312e7a7
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-06-17 15:47:53 +0530

    Done till for structural directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'Angular Tutorial';
+  colors:string[] = ["Red", "Blue", "Green"];
 }

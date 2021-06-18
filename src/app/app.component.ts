19ece871aa4e5401bb30dc9027aba5a972155645
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-06-18 19:30:33 +0530

    Iterating through indices and custom types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Car } from '../utilities/car';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Tutorial';
-  colors:string[] = ["Red", "Blue", "Green"];
+  colors: string[] = ["Red", "Blue", "Green"];
+  subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+  cars: Car[] = [this.subaru, this.honda, this.bmw];
 }
